89f8b50d52aa1a591c7804e70744acc2774c6553
yfg1995 <andonovic995@gmail.com>
2023-08-19 14:27:24 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-// import { Exercise1 } from "./exercises/exercise1/Exercise1";
+import { Exercise1 } from "./exercises/exercise1/Exercise1";
 import { Exercise2 } from "./exercises/exercise2/Exercise2";
 
 function App() {
   return (
     <>
-      {/* <Exercise1 /> */}
+      <Exercise1 />
       <Exercise2 />
     </>
   );
--- src/components/SelectDropdown.tsx
+++ src/components/SelectDropdown.tsx
@@ -1,4 +1,5 @@
 import { useState, FC, CSSProperties } from "react";
+import { classNames } from "../helpers/helpers";
 
 export type TSelectOption = {
   value: string;
@@ -42,7 +43,7 @@ export const SelectDropdown: FC<ISelect> = ({
     <div className="relative mr-5 cursor-pointer">
       <div
         style={{ borderRadius: "10px 10px 0 0" }}
-        className="relative p-2.5 w-1/2 text-lg border border-slate-300"
+        className="relative flex justify-between items-center p-2.5 w-1/2 text-lg border border-slate-300"
         onClick={handleToggle}
       >
         {name && (
@@ -51,6 +52,24 @@ export const SelectDropdown: FC<ISelect> = ({
           </div>
         )}
         {options.find((option) => option.id === selectedId)?.value}
+
+        <svg
+          viewBox="0 0 511.735 511.735"
+          className={classNames(
+            "transition-all h-3 w-3 mr-1.5",
+            toggle ? "rotate-180" : ""
+          )}
+        >
+          <g>
+            <g>
+              <path
+                d="M508.788,371.087L263.455,125.753c-4.16-4.16-10.88-4.16-15.04,0L2.975,371.087c-4.053,4.267-3.947,10.987,0.213,15.04
+			c4.16,3.947,10.667,3.947,14.827,0l237.867-237.76l237.76,237.76c4.267,4.053,10.987,3.947,15.04-0.213
+			C512.734,381.753,512.734,375.247,508.788,371.087z"
+              />
+            </g>
+          </g>
+        </svg>
       </div>
 
       {toggle && (
